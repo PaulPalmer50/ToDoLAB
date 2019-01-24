8dcba864bc64de32e21552585fd0fa255e79d99c
Paul Palmer <paulpalmer50@gmail.com>
2019-01-24 20:35:21 +1100

    Upgraded data persistence with NSCoder and data to plist files.

--- ToDoLAB/Contollers/TodoListViewController.swift
+++ ToDoLAB/Contollers/TodoListViewController.swift
@@ -8,18 +8,20 @@
 
 import UIKit
 
+
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["One", "Two", "Three"]
-    
-    let defaults = UserDefaults.standard
+    var itemArray = [Item]()
+
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+        print(dataFilePath!)
+        
+        loadItems()
+
     }
 
     //MARK: - TableView Datasource Methods
@@ -32,7 +34,11 @@ class TodoListViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
     }
 
@@ -40,13 +46,9 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        print(itemArray[indexPath.row])
-
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-           tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -62,8 +64,11 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will occur on add click
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
             
             self.tableView.reloadData()
             
@@ -78,6 +83,30 @@ class TodoListViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    func saveItems() {
+    
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error saving item \(error)")
+        }
+        
+    }
+    
+    func loadItems() {
+
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding data \(error)")
+            }
+        }
+    }
     
 }
 
---
+++ ToDoLAB/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ToDoLAB
+//
+//  Created by Paul Palmer on 24/1/19.
+//  Copyright © 2019 Paul Palmer. All rights reserved.
+//
+
+import Foundation
+
+class Item : Codable {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
